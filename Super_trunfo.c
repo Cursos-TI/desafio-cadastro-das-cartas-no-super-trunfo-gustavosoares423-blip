4b7203e05d2195d24d284b1a676d110883b7326f
Gustavo Martins <gustavosoares423@gmail.com>
2025-08-13 19:17:43 +0000

    NOVO commit

---
+++ Super_trunfo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+    int main() {
+    printf("Super Trunfo - Paises\n");
+
+    char Estado;
+    char Codigo[10];
+    char Cidade[50];
+    int Populaçao;
+    float Area;
+    float PIB;
+    int PontosT;
+
+    printf("Digite o Estado: \n");
+    scanf("%c, &Nome");
+
+    printf("Digite o Codigo: \n");
+    scanf("%s, Codigo");
+
+    printf("Digite a Cidade: \n");
+    scanf("%s, Cidade");
+
+    }
